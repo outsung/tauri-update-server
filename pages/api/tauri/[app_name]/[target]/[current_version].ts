1080cae9464269afd2425b6a2bf4e199004897ae
Junseo Oh <insung9546@gmail.com>
2022-12-03 19:18:53 +0900

    Feat:  add 0.0.2 update response

--- pages/api/tauri/[app_name]/[target]/[current_version].ts
+++ pages/api/tauri/[app_name]/[target]/[current_version].ts
@@ -8,15 +8,21 @@ export default function assetsEndpoint(
   const target = req.query.target;
   const currentVersion = req.query.current_version;
 
-  // const url = `http://localhost:3000/api/${appName}`;
   console.log({ appName, target, currentVersion });
 
-  res.statusCode = 204;
-  res.json({});
-  // res.json({
-  //   url: "https://mycompany.example.com/myapp/releases/myrelease.tar.gz",
-  //   version: "0.0.1",
-  //   signature: "",
-  // });
-  return;
+  if (currentVersion === "0.0.1") {
+    res.statusCode = 200;
+    res.json({
+      url: "https://github.com/outsung/tauri-update-server/raw/main/updates/tauri/0.0.2/tauri-boilerplate.app.tar.gz",
+      version: "0.0.2",
+      notes: "v0.0.2 test file",
+      pub_date: "2020-09-18T12:29:53+01:00",
+      signature:
+        "dW50cnVzdGVkIGNvbW1lbnQ6IG1pbmlzaWduIHB1YmxpYyBrZXk6IEExODNDOTg1QUU0NDIyMEQKUldRTklrU3VoY21Eb2ZqeHdWS29RWWlvOGw2b0Zid2FnenJ2aDkwU1hkdXU1ZjdoUWZ6UnIzRVYK",
+    });
+  } else {
+    res.statusCode = 204;
+    res.json({});
+    return;
+  }
 }
